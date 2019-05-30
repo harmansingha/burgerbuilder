76cc1d20e841bea3e0eed9567719b0f9ad40f5fa
Kumar <HKumar4@ad.harman.com>
2019-05-31 00:08:31 +0530

    Toolbar Changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Auxilary from '../../hoc/Auxilary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => {
     return (
         <Auxilary>
-            <div>Toolbar, NagigationDrawaer, BackDrop</div>
+            <Toolbar />
             <main className={classes.Content}>{props.children}</main>
         </Auxilary>
     )
