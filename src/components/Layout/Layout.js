e8cf0f73288d0f344cf72375f43f064204ef41d1
Harman Kumar (c) <harmank@vmware.com>
2019-05-31 10:26:11 +0530

    SideDrawer Code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Auxilary from '../../hoc/Auxilary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const Layout = (props) => {
     return (
         <Auxilary>
             <Toolbar />
+            <SideDrawer />
             <main className={classes.Content}>{props.children}</main>
         </Auxilary>
     )
